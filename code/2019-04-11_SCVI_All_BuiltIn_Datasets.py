14c6b224a70d69f2f9dbfb17f0ff7ce3061e19dd
10098777 <lihui10098777@hotmail.com>
2019-05-07 15:05:18 +1000

    not important
    Signed-off-by: 10098777 <lihui10098777@hotmail.com>

--- code/2019-04-11_SCVI_All_BuiltIn_Datasets.py
+++ code/2019-04-11_SCVI_All_BuiltIn_Datasets.py
@@ -13,6 +13,7 @@ This is a temporary script file.
 """
 
 import os
+
 os.getcwd()
 if not os.path.exists('./data/2019-04-11'):
     os.makedirs('./data/2019-04-11')
@@ -23,33 +24,32 @@ from scvi.dataset import *
 import numpy as np
 import pandas as pd
 
-#%matplotlib inline
+# %matplotlib inline
 from scvi.models import *
 from scvi.inference import UnsupervisedTrainer
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import torch
 
-
 hemato_dataset = HematoDataset(save_path=os.path.join(save_path, 'HEMATO/'))
 
 n_epochs_all = None
 show_plot = True
 
-n_epochs=200 if n_epochs_all is None else n_epochs_all
-lr=0.0004
-use_batches=False
-use_cuda=True
+n_epochs = 200 if n_epochs_all is None else n_epochs_all
+lr = 0.0004
+use_batches = False
+use_cuda = True
 
-# train the model 
+# train the model
 hemato_vae = VAE(hemato_dataset.nb_genes, n_batch=hemato_dataset.n_batches * use_batches)
 hemato_trainer = UnsupervisedTrainer(hemato_vae,
                                      hemato_dataset,
-                                     train_size=0.9, 
-                                     use_cuda=use_cuda, 
+                                     train_size=0.9,
+                                     use_cuda=use_cuda,
                                      frequency=5)
 hemato_trainer.train(n_epochs=n_epochs, lr=lr)
-    
+
 asw, nmi, ari, uca = hemato_trainer.train_set.clustering_scores()
 
 smfish_dataset = SmfishDataset(save_path=save_path)
