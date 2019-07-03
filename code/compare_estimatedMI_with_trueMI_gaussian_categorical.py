9723e815d5a4a667f818dbce648b90d641316f09
10098777 <lihui10098777@hotmail.com>
2019-07-03 10:28:23 +1000

    scVI+MINE on MouseMarrow dataset from HarmonizationSCANVI
    Signed-off-by: 10098777 <lihui10098777@hotmail.com>

--- code/compare_estimatedMI_with_trueMI_gaussian_categorical.py
+++ code/compare_estimatedMI_with_trueMI_gaussian_categorical.py
@@ -20,7 +20,7 @@ from tqdm import tqdm
 
 hyperparameter_config = {
         'method': ['Mine_Net','nearest_neighbor'],
-        'gaussian_dimension': [2],
+        'gaussian_dimension': [2,4],
         'repos': [100]
     }
 keys, values = zip(*hyperparameter_config.items())
@@ -44,38 +44,43 @@ def y_pdf_entropy_2(y1, y2):
             np.matmul(np.matmul(y_center, np.linalg.inv(cov_mat)), np.transpose(y_center)) * (-0.5))))
     return -y_pdf * math.log(y_pdf)
 
+def y_pdf_entropy_4(y1, y2, y3,y4):
+    gaussian_dim = 4
+    y_pdf = 0
+    for i in range(len(p_list)):
+        y_center = np.array([[y1, y2, y3, y4]]) - np.array([[mu_list[i]] * gaussian_dim])
+        cov_mat = ((sigma_list[i]) ** 2) * np.identity(gaussian_dim)
+        y_pdf += p_list[i] * ((1 / (math.sqrt((2 * math.pi) ** gaussian_dim) * math.sqrt(np.linalg.det(cov_mat))) * math.exp(
+            np.matmul(np.matmul(y_center, np.linalg.inv(cov_mat)), np.transpose(y_center)) * (-0.5))))
+    return -y_pdf * math.log(y_pdf)
+
 final_dataframe = pd.DataFrame(columns=['method', 'distribution', 'distribution_dimension', 'gaussian_dimension', 'sample_size','train_size', 'rho', 'true_MI', 'training_or_testing', 'estimated_MI', 'final_loss', 'standard_deviation'])
 
-for taskid in range(len(hyperparameter_experiments)):
-    key, value = zip(*hyperparameter_experiments[taskid].items())
-    method = value[0]
-    gaussian_dimension = value[1]
-    repos = value[2]
+# distribution of categorical variables
+p_array = np.array(
+    [[0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+     [0.01, 0.03, 0.05, 0.07, 0.09, 0.11, 0.13, 0.15, 0.17, 0.19],
+     [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+     [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+     [0.01, 0.01, 0.01, 0.01, 0.01, 0.03, 0.2, 0.2, 0.3, 0.22]])
 
-    if gaussian_dimension == 2:
-        # distribution of categorical variables
-        p_array = np.array(
-            [[0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
-             [0.01, 0.03, 0.05, 0.07, 0.09, 0.11, 0.13, 0.15, 0.17, 0.19],
-             [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
-             [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
-             [0.01, 0.01, 0.01, 0.01, 0.01, 0.03, 0.2, 0.2, 0.3, 0.22]])
+# center of the gaussian disribution
+mu_array = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 2, 4, 6, 8, 10, 12, 14, 16, 18],
+                     [0, 0, 100, 100, 200, 200, 0, 0, 0, 0],
+                     [0, 1, 2, 2, 2, 3, 3, 3, 3, 4], [0, 2, 4, 0, 0, 2, 0, 0, 0, 0],
+                     [0, 20, 40, 60, 80, 100, 120, 140, 160, 180],
+                     [0, 20, 40, 60, 80, 100, 120, 140, 160, 180]])
 
-        # center of the gaussian disribution
-        mu_array = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 2, 4, 6, 8, 10, 12, 14, 16, 18],
-                             [0, 0, 100, 100, 200, 200, 0, 0, 0, 0],
-                             [0, 1, 2, 2, 2, 3, 3, 3, 3, 4], [0, 2, 4, 0, 0, 2, 0, 0, 0, 0],
-                             [0, 20, 40, 60, 80, 100, 120, 140, 160, 180],
-                             [0, 20, 40, 60, 80, 100, 120, 140, 160, 180]])
+# the cov matrix of the gaussian distribution
 
-        # the cov matrix of the gaussian distribution
-
-        sigma_array = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                                [1, 1, 20, 20, 40, 40, 1, 1, 1, 1],
-                                [1, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                                [1, 2, 3, 4, 5, 5, 8, 8, 10, 10], [1, 2, 3, 4, 5, 5, 8, 8, 10, 10]])
-
-    for iteration in range(p_array.shape[0]):
+sigma_array = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                        [1, 1, 20, 20, 40, 40, 1, 1, 1, 1],
+                        [1, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                        [1, 2, 3, 4, 5, 5, 8, 8, 10, 10], [1, 2, 3, 4, 5, 5, 8, 8, 10, 10]])
+true_MI2 = []
+true_MI4 = []
+for iteration in range(p_array.shape[0]):
+    for gaussian_dimension in [4]:
         p_list = p_array[iteration, :]
         cum_p = np.cumsum(p_list)
         mu_list = mu_array[iteration, :]
@@ -84,7 +89,11 @@ for taskid in range(len(hyperparameter_experiments)):
         y_min = min((np.array([mu_list]) - 10 * np.array([sigma_list])).ravel())
         y_max = max((np.array([mu_list]) + 10 * np.array([sigma_list])).ravel())
 
-        y_entropy, y_entropy_err = nquad(y_pdf_entropy_2, [[y_min, y_max], [y_min, y_max]])
+        if gaussian_dimension == 2:
+            y_entropy, y_entropy_err = nquad(y_pdf_entropy_2, [[y_min, y_max], [y_min, y_max]])
+        else:
+            y_entropy, y_entropy_err = nquad(y_pdf_entropy_4, [[y_min, y_max], [y_min, y_max], [y_min, y_max], [y_min, y_max]])
+
         intermediate_entropy = 0
         for i in range(len(p_list)):
             cov_mat = ((sigma_list[i]) ** 2) * np.identity(gaussian_dimension)
@@ -92,8 +101,24 @@ for taskid in range(len(hyperparameter_experiments)):
 
         y_given_x_entropy = gaussian_dimension / 2 + gaussian_dimension / 2 * math.log(2 * math.pi) + 1 / 2 * intermediate_entropy
         true_MI = y_entropy - y_given_x_entropy
+        if gaussian_dimension==2:
+            true_MI2 += [true_MI]
+        else:
+            true_MI4 += [true_MI]
 
-        if method == 'Mine_Net':
+for taskid in range(len(hyperparameter_experiments)):
+    key, value = zip(*hyperparameter_experiments[taskid].items())
+    method = value[0]
+    gaussian_dimension = value[1]
+    repos = value[2]
+
+    for iteration in range(p_array.shape[0]):
+        p_list = p_array[iteration, :]
+        cum_p = np.cumsum(p_list)
+        mu_list = mu_array[iteration, :]
+        sigma_list = sigma_array[iteration, :]
+
+        if gaussian_dimension==2 and method == 'Mine_Net':
             sample_size = 14388
             train_size = 0.5
             x_array = np.empty((1, sample_size), int)
@@ -171,10 +196,18 @@ for taskid in range(len(hyperparameter_experiments)):
                 estimated_MI = torch.Tensor.cpu(estimated_MI).detach().numpy().item()
                 dict = {'method': [method], 'distribution': ['categorical'], 'distribution_dimension': [1],
                         'gaussian_dimension': [gaussian_dimension], 'sample_size': [sample_size],
-                        'train_size': [train_size],'rho': [None], 'true_MI': [true_MI], 'training_or_testing': [type],
+                        'train_size': [train_size],'rho': [None], 'true_MI': [true_MI2[iteration]], 'training_or_testing': [type],
                         'estimated_MI': [estimated_MI],'final_loss': [None], 'standard_deviation': [None]}
                 intermediate_dataframe = pd.DataFrame.from_dict(dict)
                 final_dataframe = pd.concat([final_dataframe, intermediate_dataframe])
+        elif gaussian_dimension == 4 and method == 'Mine_Net':
+            dict = {'method': [method], 'distribution': ['categorical'], 'distribution_dimension': [1],
+                    'gaussian_dimension': [gaussian_dimension], 'sample_size': [None],
+                    'train_size': [None], 'rho': [None], 'true_MI': [true_MI4[iteration]], 'training_or_testing': [None],
+                    'estimated_MI': [None], 'final_loss': [None], 'standard_deviation': [None]}
+            intermediate_dataframe = pd.DataFrame.from_dict(dict)
+            final_dataframe = pd.concat([final_dataframe, intermediate_dataframe])
+
         elif method == 'nearest_neighbor':
             estimatedMI_NN_list = [] #nn means nearest neighbor method
             sample_size = 128
@@ -192,10 +225,13 @@ for taskid in range(len(hyperparameter_experiments)):
                 estimatedMI_NN_list += [one_dc_info_dim]
             mean_estimatedMI_NN = statistics.mean(estimatedMI_NN_list)
             std_estimatedMI_NN = statistics.stdev(estimatedMI_NN_list)
-
+            if gaussian_dimension==2:
+                true_MI_selected = true_MI2[iteration]
+            else:
+                true_MI_selected = true_MI4[iteration]
             dict = {'method': [method], 'distribution': ['categorical'], 'distribution_dimension': [1],
                 'gaussian_dimension': [gaussian_dimension], 'sample_size': [sample_size], 'train_size': [None],
-                'rho': [None], 'true_MI': [true_MI], 'training_or_testing': [None], 'estimated_MI': [mean_estimatedMI_NN],
+                'rho': [None], 'true_MI': [true_MI_selected], 'training_or_testing': [None], 'estimated_MI': [mean_estimatedMI_NN],
                 'final_loss': [None],'standard_deviation':[std_estimatedMI_NN]}
             intermediate_dataframe = pd.DataFrame.from_dict(dict)
             final_dataframe = pd.concat([final_dataframe, intermediate_dataframe])
---
+++ code/tune_hyperparameter_for_MineNet_MonteCarlo.py
@@ -0,0 +1,160 @@
+#Run 100 Monte Carlo Samples for each dataset, as scVI is variable for each run.
+import os
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scvi.dataset import *
+from scvi.dataset.dataset import GeneExpressionDataset
+from scvi.dataset.muris_tabula import TabulaMuris
+from scvi.models import *
+from scvi.inference import UnsupervisedTrainer
+import torch
+import itertools
+
+def main(taskid):
+    if not os.path.exists('data/tune_hyperparameter_for_MineNet/muris_tabula'):
+        os.makedirs('data/tune_hyperparameter_for_Minenet/muris_tabula')
+    if not os.path.exists('result/tune_hyperparameter_for_Minenet/muris_tabula'):
+        os.makedirs('result/tune_hyperparameter_for_Minenet/muris_tabula')
+
+    hyperparameter_config = {
+        'dataset_name': ['Marrow'],
+        'nuisance_factor': ['batch'],
+        'MineLoss_Scale': [200, 500, 800, 1000, 2000, 5000]
+    }
+    keys, values = zip(*hyperparameter_config.items())
+    hyperparameter_experiments = [dict(zip(keys, v)) for v in itertools.product(*values)]
+
+    save_path = './data/tune_hyperparameter_for_Minenet/muris_tabula'
+
+    #gene_dataset = PbmcDataset(save_path=save_path)
+    #gene_dataset = RetinaDataset(save_path=save_path)
+    dataset1 = TabulaMuris('facs',save_path = save_path)
+    dataset2 = TabulaMuris('droplet',save_path = save_path)
+    dataset1.subsample_genes(dataset1.nb_genes)
+    dataset2.subsample_genes(dataset2.nb_genes)
+    gene_dataset = GeneExpressionDataset.concat_datasets(dataset1, dataset2)
+
+    np.random.seed(1011)
+    desired_seeds = np.random.randint(0, 2 ** 32, size=(1, 100),dtype=np.uint32)
+
+    taskid = int(taskid[0])
+    desired_seed = int(desired_seeds[0,taskid])
+
+    n_epochs_all = None
+    show_plot = True
+    n_epochs = 400 if n_epochs_all is None else n_epochs_all
+    lr = 0.0005
+    use_batches = True
+    use_cuda = False
+    train_size = 0.6
+
+    n_samples_tsne = 1000
+    clustering_metric = pd.DataFrame(columns=['Label', 'asw', 'nmi', 'ari', 'uca', 'be'])
+
+    for i in range(len(hyperparameter_experiments)):
+        key, value = zip(*hyperparameter_experiments[i].items())
+        dataset_name = value[0]
+        nuisance_variable = value[1]
+        MineLoss_Scale = value[2]
+
+        vae_mine = VAE_MINE(gene_dataset.nb_genes, n_batch=gene_dataset.n_batches * use_batches, MineLoss_Scale=MineLoss_Scale)
+        trainer_vae_mine = UnsupervisedTrainer(vae_mine, gene_dataset, train_size=train_size, seed=desired_seed, use_cuda=use_cuda,frequency=5, kl=1)
+        vae_mine_file_path = '%s/%s_%s_MineLossScale%s_sample%s_VaeMine.pk1'%(save_path, dataset_name, nuisance_variable, MineLoss_Scale, taskid)
+
+        if os.path.isfile(vae_mine_file_path):
+            trainer_vae_mine.model.load_state_dict(torch.load(vae_mine_file_path))
+            trainer_vae_mine.model.eval()
+        else:
+            trainer_vae_mine.train(n_epochs=n_epochs, lr=lr)
+            torch.save(trainer_vae_mine.model.state_dict(), vae_mine_file_path)
+            ll_train_set = trainer_vae_mine.history["ll_train_set"]
+            ll_test_set = trainer_vae_mine.history["ll_test_set"]
+            x = np.linspace(0, 500, (len(ll_train_set)))
+
+            fig = plt.figure(figsize=(14, 7))
+            plt.plot(x, ll_train_set)
+            plt.plot(x, ll_test_set)
+            plt.ylim(1150, 1600)
+            plt.title("Blue for training error and orange for testing error")
+
+            fig1_path = 'result/tune_hyperparameter_for_Minenet/muris_tabula/training_testing_error_SCVI+MINE_{}_{}_sample{}_MineLossScale{}.png'.format(dataset_name,nuisance_variable,taskid,MineLoss_Scale)
+            fig.savefig(fig1_path)
+            plt.close(fig)
+
+        trainer_vae_mine.train_set.show_t_sne(n_samples_tsne, color_by='batches and labels',save_name='result/tune_hyperparameter_for_Minenet/muris_tabula/trainset_tsne_SCVI+MINE_{}_{}_sample{}_MineLossScale{}'.format(dataset_name,nuisance_variable, taskid, MineLoss_Scale))
+        trainer_vae_mine.test_set.show_t_sne(n_samples_tsne, color_by='batches and labels', save_name='result/tune_hyperparameter_for_Minenet/muris_tabula/testset_tsne_SCVI+MINE_{}_{}_sample{}_MineLossScale{}'.format(dataset_name,nuisance_variable, taskid, MineLoss_Scale))
+
+        asw, nmi, ari, uca = trainer_vae_mine.train_set.clustering_scores()
+        be = trainer_vae_mine.train_set.entropy_batch_mixing()
+        label = '%s_%s_sample%s_MineLossScale%s_VaeMine_trainset'%(dataset_name, nuisance_variable, taskid, MineLoss_Scale)
+        intermediate_dataframe1 = pd.DataFrame.from_dict({'Label':[label],'asw':[asw],'nmi':[nmi],'ari':[ari],'uca':[uca],'be':[be]})
+        clustering_metric = pd.concat([clustering_metric, intermediate_dataframe1], axis=0)
+
+        asw, nmi, ari, uca = trainer_vae_mine.test_set.clustering_scores()
+        be = trainer_vae_mine.test_set.entropy_batch_mixing()
+        label = '%s_%s_sample%s_MineLossScale%s_VaeMine_testset'%(dataset_name, nuisance_variable, taskid, MineLoss_Scale)
+        intermediate_dataframe2 = pd.DataFrame.from_dict({'Label': [label], 'asw': [asw], 'nmi': [nmi], 'ari': [ari], 'uca': [uca], 'be': [be]})
+        clustering_metric = pd.concat([clustering_metric, intermediate_dataframe2], axis=0)
+
+    vae = VAE(gene_dataset.nb_genes, n_batch=gene_dataset.n_batches * use_batches)
+    trainer_vae = UnsupervisedTrainer(vae, gene_dataset, train_size=train_size, seed=desired_seed, use_cuda=use_cuda, frequency=5)
+    vae_file_path = '%s/%s_%s_sample%s_Vae.pk1'%(save_path,dataset_name,nuisance_variable, taskid)
+
+    if os.path.isfile(vae_file_path):
+        trainer_vae.model.load_state_dict(torch.load(vae_file_path))
+        trainer_vae.model.eval()
+    else:
+        trainer_vae.train(n_epochs=n_epochs, lr=lr)
+        torch.save(trainer_vae.model.state_dict(), vae_file_path)
+
+    ll_train_set = trainer_vae.history["ll_train_set"]
+    ll_test_set = trainer_vae.history["ll_test_set"]
+    x = np.linspace(0, 500, (len(ll_train_set)))
+
+    fig = plt.figure(figsize=(14, 7))
+    plt.plot(x, ll_train_set)
+    plt.plot(x, ll_test_set)
+    plt.ylim(1150, 1600)
+    plt.title("Blue for training error and orange for testing error")
+
+    fig2_path = 'result/tune_hyperparameter_for_Minenet/muris_tabula/training_testing_error_SCVI_{}_{}_sample{}.png'.format(dataset_name, nuisance_variable, taskid)
+    fig.savefig(fig2_path)
+    plt.close(fig)
+
+    trainer_vae.train_set.show_t_sne(n_samples_tsne, color_by='batches and labels', save_name='result/tune_hyperparameter_for_Minenet/muris_tabula/trainset_tsne_SCVI_{}_{}_sample{}'.format(dataset_name,nuisance_variable,taskid))
+    trainer_vae.test_set.show_t_sne(n_samples_tsne, color_by='batches and labels', save_name='result/tune_hyperparameter_for_Minenet/muris_tabula/testset_tsne_SCVI_{}_{}_sample{}'.format(dataset_name,nuisance_variable,taskid))
+
+    # clustering_scores() -- these metrics measure clustering performance
+    #   silhouette width (asw, higher is better),
+    #   normalised mutual information (nmi, higher is better),
+    #   adjusted rand index (ari, higher is better),
+    #   unsupervised clustering accuracy (uca)
+    #   entropy_batch_mixing() -- this metric measures batch effect
+    #   entropy batch mixing (be, higher is better meaning less batch effect)
+
+
+    asw, nmi, ari, uca = trainer_vae.train_set.clustering_scores()
+    be = trainer_vae.train_set.entropy_batch_mixing()
+    label = '%s_%s_sample%s_Vae_trainset' % (dataset_name, nuisance_variable, taskid)
+    intermediate_dataframe1 = pd.DataFrame.from_dict({'Label': [label], 'asw': [asw], 'nmi': [nmi], 'ari': [ari], 'uca': [uca], 'be': [be]})
+    clustering_metric = pd.concat([clustering_metric, intermediate_dataframe1], axis=0)
+
+    asw, nmi, ari, uca = trainer_vae.test_set.clustering_scores()
+    be = trainer_vae.test_set.entropy_batch_mixing()
+    label = '%s_%s_sample%s_Vae_testset' % (dataset_name, nuisance_variable, taskid)
+    intermediate_dataframe2 = pd.DataFrame.from_dict({'Label': [label], 'asw': [asw], 'nmi': [nmi], 'ari': [ari], 'uca': [uca], 'be': [be]})
+    clustering_metric = pd.concat([clustering_metric, intermediate_dataframe2], axis=0)
+
+    clustering_metric.to_csv('result/tune_hyperparameter_for_Minenet/muris_tabula/%s_%s_sample%s_ClusterMetric.csv'%(dataset_name, nuisance_variable, taskid), index=None, header=True)
+
+# Run the actual program
+if __name__ == "__main__":
+  main(sys.argv[1:])
+
+# In terminal type
+# python hypertuning.py taskid
+# this will unpickle the list of dictionaries
+# with open("debiasing_sweep_config.txt", "rb") as fp:   # Unpickling
+#     b = pickle.load(fp)
