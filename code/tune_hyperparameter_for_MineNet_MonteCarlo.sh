50074f87333050bd96c567ead80490da637b9a44
10098777 <lihui10098777@hotmail.com>
2019-07-03 12:48:51 +1000

    compare scvi and scvi_MI on MouseMarrow dataset
    Signed-off-by: 10098777 <lihui10098777@hotmail.com>

--- code/tune_hyperparameter_for_MineNet_MonteCarlo.sh
+++ code/tune_hyperparameter_for_MineNet_MonteCarlo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for num in {0..99} ; do
-echo sbatch -p mig --job-name lihui10098777.${num} --account punim0614 --ntasks=1 --cpus-per-task=2 --mem=10000 --mail-type=END --mail-user=hui.li3@student.unimelb.edu.au --begin=now+$((0*num)) --time=2-0:0:00 -o \"slurm_output/slurm-%A_$num.out\" --wrap=\"module load Python/3.7.1-GCC-6.2.0\; source venv/bin/activate\; module load web_proxy; python tune_hyperparameter_for_MineNet_MonteCarlo.py $num \"
+echo sbatch -p mig --job-name lihui10098777.${num} --account punim0614 --ntasks=1 --cpus-per-task=2 --mem=10000 --mail-type=END --mail-user=hui.li3@student.unimelb.edu.au --begin=now+$((0*num)) --time=2-0:0:00 -o \"slurm_output/slurm-%A_$num.out\" --wrap=\"module load Python/3.7.1-GCC-6.2.0\; source venv/bin/activate\; module load web_proxy\; python tune_hyperparameter_for_MineNet_MonteCarlo.py $num \"
 sleep 1
 
 sbatch -p mig --job-name lihui10098777.${num} --account punim0614 --ntasks=1 --cpus-per-task=2 --mem=10000 --mail-type=END --mail-user=hui.li3@student.unimelb.edu.au --begin=now+$((0*num)) --time=2-0:0:00 -o "slurm_output/slurm-%A_$num.out" --wrap="module load Python/3.7.1-GCC-6.2.0; source venv/bin/activate; module load web_proxy; python tune_hyperparameter_for_MineNet_MonteCarlo.py $num"
