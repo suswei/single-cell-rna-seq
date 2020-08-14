fb574453638ac22351874d4cc15560ed12bab690
10098777 <lihui10098777@hotmail.com>
2020-08-14 17:06:06 +1000

    paretoMTL
    Signed-off-by: 10098777 <lihui10098777@hotmail.com>

--- MINE_simulation1_sweep.sh
+++ MINE_simulation1_sweep.sh
@@ -35,8 +35,6 @@ fi
 # Run the job from the directory where it was launched (default)
 
 # The job command(s):
-module load anaconda3/2020.02
-source activate sharedenv
 module load python/3.7.4
 python3 MINE_simulation1_sweep.py ${SLURM_ARRAY_TASK_ID}
 
