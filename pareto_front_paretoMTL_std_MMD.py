a8da76a0e58b33f3ce62afed8f2868977084f884
10098777 <lihui10098777@hotmail.com>
2020-12-18 14:11:57 +1100

    apply paretoMTL to new datasets get max and min for obj1 and obj2
    Signed-off-by: 10098777 <lihui10098777@hotmail.com>

--- pareto_front_paretoMTL_std_MMD.py
+++ pareto_front_paretoMTL_std_MMD.py
@@ -35,7 +35,7 @@ def main(taskid):
 
     temp = hyperparameter_experiments[taskid]
 
-    os.system("python3 pareto_front_paretoMTL_main.py --change_composition --MCs 20 "
+    os.system("python3 pareto_front_paretoMTL_main.py --change_composition --use_batches --MCs 20 "
               "--taskid %s --dataset_name %s --confounder %s --n_layers_encoder %s "
               "--n_layers_decoder %s --n_hidden %s --n_latent %s --use_batches --batch_size %s "        
               "--adv_estimator %s --MMD_kernel_mul %s --MMD_kernel_num %s "
