47e740958b4ee9b2e63fba0a45bab0a85b575dbf
10098777 <lihui10098777@hotmail.com>
2021-01-05 11:10:04 +1100

    apply paretoMTL to new datasets pre_train
    Signed-off-by: 10098777 <lihui10098777@hotmail.com>

--- pareto_front_paretoMTL_pretrain.sh
+++ pareto_front_paretoMTL_pretrain.sh
@@ -13,10 +13,10 @@
 #SBATCH --gres=gpu:p100:4
 
 # The maximum running time of the job in days-hours:mins:sec
-#SBATCH --time=0-12:0:00
+#SBATCH --time=0-18:0:00
 
 # Batch arrays
-#SBATCH --array=0-1
+#SBATCH --array=0-3
 
 # Send yourself an email when the job:
 # aborts abnormally (fails)
