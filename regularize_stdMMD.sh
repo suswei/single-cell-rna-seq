9dcbb75613d97e2edb4dc1f816c4c98df52282f3
10098777 <lihui10098777@hotmail.com>
2021-10-14 18:55:47 +1100

    tabula_muris paretoMMD regularizeMMD
    Signed-off-by: 10098777 <lihui10098777@hotmail.com>

--- regularize_stdMMD.sh
+++ regularize_stdMMD.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # The name of the job:
-#SBATCH --job-name="regularize_stdzMMD"
+#SBATCH --job-name="regularize_stdMMD"
 #SBATCH --account=punim0890
 #SBATCH -p physical
 
@@ -13,7 +13,7 @@
 #SBATCH --time=0-10:00:00
 
 # Batch arrays
-#SBATCH --array=0
+#SBATCH --array=0-239
 
 # Send yourself an email when the job:
 # aborts abnormally (fails)
